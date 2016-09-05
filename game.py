172de08471037901a354f6d720ac28d11df3bcc1
zarnold <zarnold@9online.fr>
2016-09-05 20:40:20 +0200

    find a list of number that sum to 10

---
+++ game.py
@@ -0,0 +1,40 @@
+TARGET  = 38
+
+BACK = -1
+STOP = 0
+FORWARD = 1
+
+bags=[7,5,3,1]
+
+maxSize = 10
+
+path=[]
+
+def addMoar(v):
+  path.append(v)
+  print path
+  with open("path.txt","a") as f:
+    f.write(str(path))
+    f.write("\n")
+  print("Sum: %d"%sum(path))
+  #raw_input("Press Enter to continue...")
+  print(len(path))
+  if (len(path) > 10) or (sum(path) > TARGET):
+    print("opo")
+    path.pop()
+    return BACK
+  if (len(path) == 10) and (sum(path) == TARGET):
+    return STOP
+  for v in bags:
+    print(v)
+    rep = addMoar(v)
+    if rep == FORWARD: 
+      continue
+    if rep == STOP:
+      return STOP
+  path.pop()
+  return BACK
+
+
+addMoar(7)
+
